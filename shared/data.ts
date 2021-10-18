f5610ddbbbbde90de6505002532bc4bb1e787e66
luisalbertodev <luisalbertodev@gmail.com>
2021-10-17 21:34:13 -0500

    added model to user and post

---
+++ shared/data.ts
@@ -0,0 +1,8 @@
+export const USERS = [
+   { id: '1', name: 'Leanne' },
+   { id: '2', name: 'Juan perez' },
+   { id: '3', name: 'Miguel Aleman' },
+   { id: '4', name: 'Karmen Jimenez' },
+]
+
+export const BASE_URL = 'https://jsonplaceholder.typicode.com'
---
+++ shared/models/user.model.ts
@@ -0,0 +1,28 @@
+export interface IAddress {
+   street: string;
+   suite: string;
+   city: string;
+   zipcode: string;
+   geo: {
+      lat: string
+      lng: string
+   }
+}
+
+export interface ICompany {
+   name: string;
+   catchPhrase: string;
+   bs: string;
+}
+
+export interface IUsers {
+   id: number;
+   name: string;
+   username: string;
+   email: string;
+   address: IAddress
+   phone: string;
+   website: string;
+   company: ICompany
+
+}
