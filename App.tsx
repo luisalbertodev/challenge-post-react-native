7b868a714f15d7f6f8cfd7bbe0b39d42606a4452
luisalbertodev <luisalbertodev@gmail.com>
2021-10-17 21:34:38 -0500

    added navigation to app

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import UsersScreen from "./screens/UsersScreen";
+import PostsScreen from "./screens/PostsScreen";
+import DetailScreen from "./screens/DetailScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AppNavigator = createStackNavigator(
+  {
+    Users: {
+      screen: UsersScreen,
+    },
+    Posts: {
+      screen: PostsScreen,
+    },
+    Detail: {
+      screen: DetailScreen,
+    },
   },
-});
+  {
+    initialRouteName: "Users",
+  }
+);
+
+export default createAppContainer(AppNavigator);
